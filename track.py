0787f1e453be5295ab1c137a5949a0ab4929ae91
Jeremy Watt <jermwatt@gmail.com>
2023-06-03 20:57:54 -0700

    init commit

---
+++ track.py
@@ -0,0 +1,19 @@
+import cv2
+from ultralytics import YOLO
+
+
+def main():
+    # import model
+    model = YOLO('YOLOv8s-seg.pt')
+
+    # act and track
+    for result in model.track(source=0, show=True, stream=True):
+        frame = result.orig_img
+        cv2.imshow('yolo8', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+if __name__ == '__main__':
+    main()
