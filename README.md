008eb24d128209b53d68858929f3de5eab23cc3c
Jeremy Watt <jermwatt@gmail.com>
2023-06-04 18:41:02 -0700

    simple yolo8 local demo init

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Setup for usage of yolo with lap for tracking
 Step 1: create conda env with python 3.8
 
 ```bash 
-conda create --name testies python=3.8 
+conda create --name test python=3.8 
 ```
 
 Step 2: activate conda env  
 
 ```bash  
-conda activate testies
+conda activate test
 ```
 
 Step 3: install requirements 
@@ -18,7 +18,10 @@ Step 3: install requirements
 pip install -r requirements.txt
 ```
 
-Step 4: start tracker
+Step 4: download a pretrained yolo8 model and place it in the `/models` directory.  For example, [yolo8 segmentation models](https://docs.ultralytics.com/tasks/segment/).
+
+
+Step 5: start tracker
 
 ```python 
 python track.py
--- track.py
+++ track.py
@@ -7,17 +7,17 @@ current_dir = os.getcwd()
 def main():
     # import model
     model = YOLO(current_dir + '/models/YOLOv8n-seg.pt')
+    
+    # simple streaming demo
+    result = model.track(source=0, imgsz=(480), show=True)
 
-    # act and track
-    for result in model.track(source=0,
-                              show=True,
-                              stream=True,
-                              imgsz=(480)):
-        frame = result.orig_img
-        cv2.imshow('yolo8', frame)
-
-        if cv2.waitKey(1) == ord('q'):
-            break
+    # # unpack frames for further transformation
+    # for result in model.track(source=0,
+    #                           show=True,
+    #                           stream=True,
+    #                           imgsz=(480)):
+    #     frame = result.orig_img
+    #     cv2.imshow('yolo8', frame)
 
 
 if __name__ == '__main__':
