978bfaa82587f0a8aed8e41f3b925d0e111ae90a
Jeremy Watt <jermwatt@gmail.com>
2023-06-08 16:01:01 -0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,5 +1,21 @@
-# pull some images from the web
 import urllib.request
+from PIL import Image
+import matplotlib.pyplot as plt
 
 def download_file(url, output_path):
     urllib.request.urlretrieve(url, output_path)
+    
+def show_img(image_path):
+    # Open the image file
+    image = Image.open(image_path)
+
+    # Display the image
+    plt.imshow(image)
+    plt.axis('off')  
+    plt.show()
+
+
+
+
+
+
